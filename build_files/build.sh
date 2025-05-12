e9417c05d95a1c2988da40e206daa7d5294fb2e4
BlueOz94 <62311122+BlueOz94@users.noreply.github.com>
2025-05-12 04:48:01 +0300

    build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
+# Install extra packages
+dnf5 install -y tmux
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
+# Enable systemd services
 systemctl enable podman.socket
+
+# Add the Linux Surface repo
+curl -o /etc/yum.repos.d/linux-surface.repo https://pkg.repo.linuxsurface.com/fedora/l>
+
+# Replace the default Fedora kernel with Linux Surface kernel
+rpm-ostree override remove \
+  kernel \
+  kernel-core \
+  kernel-modules \
+  kernel-modules-extra \
+  libwacom \
+  libwacom-data
+
+rpm-ostree install \
+  kernel-surface \
+  iptsd \
+  libwacom-surface \
+  libwacom-surface-data
