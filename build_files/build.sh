d871b9f9fc941395acff51e06e277314ca38c63c
BlueOz94 <62311122+BlueOz94@users.noreply.github.com>
2025-05-12 05:19:17 +0300

    build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
+
 set -ouex pipefail
 
-### Install extra packages
-dnf5 install -y tmux
+### Install packages
 
-### Enable systemd services
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf5 install -y tmux 
+
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+#### Example for enabling a System Unit File
+
 systemctl enable podman.socket
-
-### Add Surface kernel support
-
-# Remove Fedora's default kernel and libwacom
-rpm-ostree override remove \
-  kernel \
-  kernel-core \
-  kernel-modules \
-  kernel-modules-extra \
-  libwacom \
-  libwacom-data
-
-# Add the Linux Surface repository
-cat > /etc/yum.repos.d/linux-surface.repo << EOF
-[linux-surface]
-name=Linux Surface Kernel
-baseurl=https://pkg.repo.linuxsurface.com/fedora/\$releasever/\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://pkg.repo.linuxsurface.com/keys/3DFA4A0333A3D3B3.gpg
-EOF
-
-# Install Surface kernel and related packages
-dnf5 install -y \
-  kernel-surface \
-  iptsd \
-  libwacom-surface \
-  libwacom-surface-data
